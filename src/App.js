1127a577a44a976c4bd4adbb20ed0c639460a111
Ethel3 <akrasiethel4@gmail.com>
2022-08-04 13:45:17 +0000

    update

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ function App() {
     <div className="App">
       <div className="weather">
         <Input />
-        <div className="weather_location">
-          <h3>Weather in London</h3>
+        <div >
+          <h3 className="weather_location">Weather in London</h3>
         </div>
 
         <div className="weather_degrees">12.84°C</div>
-        <div className="weather_Description">
-          <div className="weather_Description_head">
-            <div>
-              <span>☁</span>
+        <div className="weather_description">
+          <div>
+            <div className="weather_description_head">
+              <span className="weather_icon">☁</span>
               <h3>Partly Cloudy</h3>
             </div>
             <h3>Humidity: 82%</h3>
             <h3>Wind speed: 5.66 m/s</h3>
           </div>
           <h3>JP</h3>
-          <h3>4/05/2022, 4:05:56PM</h3>
+          <h3>4/05/2022, 4:05:56 PM</h3>
         </div>
       </div>
     </div>
