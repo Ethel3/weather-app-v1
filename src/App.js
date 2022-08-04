a435d84268ecc1c265cd32911dfcfa3bc0f20631
Ethel3 <akrasiethel4@gmail.com>
2022-08-04 00:41:19 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Input from './components/Input';
+
 
 function App() {
   return (
     <div className="App">
        <div className="weather">
+        <Input />
 
        </div>
     </div>
