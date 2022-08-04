b624ba7eb193258b9312be9f2bed2fb4b7249c9f
Ethel3 <akrasiethel4@gmail.com>
2022-08-04 22:35:51 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Input from "./components/Input";
 
 function App() {
+
+  const [degrees, setDegrees] = useState(null)
+  const API_KEY = "7dd052a7ba40dd6d44a1c1eda148339c";
+
+  const fetchData = async () => {
+    const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=${API_KEY}&units=metric`)
+    const data = await res.data
+
+    console.log(data)
+  }
+useEffect(() =>{
+fetchData()
+}, [])
   return (
     <div className="App">
       <div className="weather">
         <Input />
-        <div>
-          <h3 className="weather_location">Weather in London</h3>
+        <div className="weather_display">
+          <h3 className="weather_location">Weather in Tokyo</h3>
         </div>
 
         <div className="weather_degrees">12.84°C</div>
@@ -23,7 +38,7 @@ function App() {
 
           <div className="weather_country">
             <h3>JP</h3>
-            <h3 className="weather_date">4/05/2022, 4:05:56 PM</h3>
+            <h3 className="weather_date">8/04/2022, 10:19:56 PM</h3>
           </div>
         </div>
       </div>
