0aed9c0f09eb8707a0f9f4eb4962362a7a58b5e8
Ethel3 <akrasiethel4@gmail.com>
2022-08-05 02:04:00 +0000

    update

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ function App() {
   const [location, setLocation] = useState("")
   const [description, setDescription] = useState("")
   const [icon, setIcon] = useState("")
-  const [Humidity, setHumidity] = useState(null)
+  const [humidity, setHumidity] = useState(null)
   const [wind, setWind] = useState(null)
   const [country, setCountry] = useState("")
   const API_KEY = "7dd052a7ba40dd6d44a1c1eda148339c";
 
   const fetchData = async () => {
-    const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=${API_KEY}&units=metric`)
+    const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=london&appid=${API_KEY}&units=metric`)
     const data = await res.data
     
     setDegrees (data.main.temp)
@@ -23,6 +23,8 @@ function App() {
     setDescription(data.weather[0].description)
     setIcon(data.weather[0].icon)
     setHumidity(data.main.humidity)
+    setWind(data.wind.speed)
+    setCountry(data.sys.country)
 
     console.log(data)
   }
@@ -34,22 +36,24 @@ fetchData()
       <div className="weather">
         <Input />
         <div className="weather_display">
-          <h3 className="weather_location">Weather in Tokyo</h3>
+          <h3 className="weather_location">Weather in {location}</h3>
         </div>
 
-        <div className="weather_degrees">12.84°C</div>
+        <div className="weather_degrees">{degrees} °C</div>
         <div className="weather_description">
           <div>
             <div className="weather_description_head">
-              <span className="weather_icon">☁</span>
-              <h3>Partly Cloudy</h3>
+              <span className="weather_icon">
+                <img src={`http://openweathermap.org/img/w/${icon}.png`} alt="weather icon"/>
+              </span>
+              <h3>{description} </h3>
             </div>
-            <h3>Humidity: 82%</h3>
-            <h3>Wind speed: 5.66 m/s</h3>
+            <h3>Humidity:{humidity}%</h3>
+            <h3>Wind speed: {wind} m/s</h3>
           </div>
 
           <div className="weather_country">
-            <h3>JP</h3>
+            <h3>{country}</h3>
             <h3 className="weather_date">8/04/2022, 10:19:56 PM</h3>
           </div>
         </div>
