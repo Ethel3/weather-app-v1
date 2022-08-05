67d588d27d3d31e579db9343178d33a250429571
Ethel3 <akrasiethel4@gmail.com>
2022-08-05 04:16:31 +0000

    updated

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,11 @@
 import "./input.css";
 import { FiSearch } from "react-icons/fi";
 
-const Input = ({text, submit}) => {
+const Input = ({text, submit, func}) => {
   return (
     <form className="input" onSubmit={submit}>
         <input  type ={"text"} placeholder="Please enter the location" className="input_value" onChange={text}/>
-        <span className="input_icon">
+        <span className="input_icon" onClick={func}>
         <FiSearch />
         </span>
 
